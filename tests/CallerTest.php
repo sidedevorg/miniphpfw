c69ede22aa7a7c317284614e35a207f94bb97080
Diego Vilariño <diego.vilarino@gmail.com>
2017-10-30 17:08:33 +0100

    add caller test

---
+++ tests/CallerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * CallerTest.
+ */
+class CallerTest extends TestCase
+{
+    /**
+     * Test call.
+     */
+    public function testCall()
+    {
+        $caller = new SideDevOrg\MiniPhpFw\Caller('DummyController', 'dummyMethod', []);
+        $this->assertTrue(is_callable($caller));
+
+        $request = \Zend\Diactoros\ServerRequestFactory::fromGlobals();
+        $response = new \Zend\Diactoros\Response();
+
+        $call = $caller->__invoke(
+            $request,
+            $response,
+            function () use ($response) {
+                return $response;
+            }
+        );
+
+        $this->assertInstanceOf('\Psr\Http\Message\ResponseInterface', $call);
+
+        $call->getBody()->rewind();
+
+        $this->assertEquals($call->getBody()->getContents(),
+            (new DummyController())->dummyMethod()
+        );
+    }
+}
---
+++ tests/helpers/DummyController.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Dummy class.
+ */
+class DummyController extends SideDevOrg\MiniPhpFw\Controller
+{
+    public function dummyMethod()
+    {
+        return 'dummy';
+    }
+}
